fcf24d2c3b8affcae796172546594a7f3df20830
vothianh <anh.nnn.1@gmail.com>
2025-08-09 21:23:54 +0700

    ôn tập 1

--- src/Test/ra.java
+++ src/Test/ra.java
@@ -1,12 +1,11 @@
 package Test;
 
 public class ra {
-
     // Toạ hàm in ra thông tin được lấy từ hàm nhap
     public static void main(String[] args) {
-        // Gọi hàm
-        nhap show = new nhap("Võ Thị Anh", 30, 6);
-        show.hienthi();
+        // Gọi hàm và biến tư fbeen kia
+        nhap hienthi = new nhap("Võ thị anh", 30);
+        hienthi.show();
 
     }
 }
